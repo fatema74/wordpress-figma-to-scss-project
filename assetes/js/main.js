4ce6e154a556d43eb70adcfd460c999a1b2f7386
fatema74 <fatema26721@gmail.com>
2024-12-21 12:20:15 +0600

    first commit

---
+++ assetes/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.faq__box').on('click', '.faq__title', function () {
+    $(this).toggleClass('active').next().slideToggle();
+
+    $('.faq__desc').not($(this).next()).slideUp(300);
+
+    $(this).siblings().removeClass('active');
+  });
+
+  $('#menu').slicknav();
+});
